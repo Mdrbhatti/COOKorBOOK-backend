a3b37fe37e0ff6d1fb2164885c49232894a30e0d
Mdrbhatti <Mdrbhatti@gmail.com>
2017-06-26 22:55:31 +0200

    feat(auth): implement route for login

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -4,6 +4,8 @@ import * as nodemailer from "nodemailer";
 import * as passport from "passport";
 import * as jwt from "jsonwebtoken";
 import { User } from "../models/User";
+import { IUser } from "../interfaces/IUser";
+import * as mongoose from 'mongoose';
 import { Request, Response, NextFunction } from "express";
 import { LocalStrategyInfo } from "passport-local";
 import { WriteError } from "mongodb";
@@ -17,28 +19,45 @@ export let postRegister = (req: Request, res: Response, next: NextFunction) => {
     const errors = req.validationErrors();
     // respond with errors
     if (errors) {
-        res.send(errors);
+        res.status(400).send(errors);
         return;
     }
     console.log("Request" + req.body.email);
 
-    var user = new User({ email: req.body.email, password: req.body.password, username: req.body.username });
-    user.save(function (err) {
+    var user: IUser = new User({ email: req.body.email, password: req.body.password, username: req.body.username });
+    user.save(function (err: mongoose.Error) {
         if (err) {
-            return next(err);
+            res.status(400).send({ message: "Username or email already exists" });
+        }
+        else {
+            // Return token
+            var payload = { id: user.id };
+            var token = jwt.sign(payload, process.env.SESSION_SECRET);
+            res.json({ message: "ok", token: token });
         }
     });
-    // Return token
-    var payload = { id: user.id };
-    var token = jwt.sign(payload, process.env.SESSION_SECRET);
-    res.json({ message: "ok", token: token });
 };
 
 export let postLogin = (req: Request, res: Response, next: NextFunction) => {
-    User.findOne({ "username": "test" }, function (err, user) {
-        var payload = { id: user.id };
-        var token = jwt.sign(payload, process.env.SESSION_SECRET);
-        res.json({ message: "ok", token: token });
+    User.findOne({ username: req.body.username }, function (err, user: IUser) {
+        if (err || !user) {
+            res.status(400).send({ message: "Invalid username or password" });
+        }
+        else {
+            user.comparePassword(req.body.password, function (err: any, isMatch: any) {
+                if (isMatch) {
+                    var payload = { id: user.id };
+                    var token = jwt.sign(payload, process.env.SESSION_SECRET);
+                    res.json({ message: "ok", token: token });
+                }
+                else {
+                    res.status(400).send({ message: "Invalid username or password" });
+                }
+                // Some funky shit
+                return null;
+            });
+
+        }
     });
 };
 
--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -3,4 +3,5 @@ export interface IUser extends mongoose.Document {
     email: string;
     password: string;
     username: string;
+    comparePassword(candidatePassword: string, cb: (err: any, isMatch: any) => {}): void;
 };
