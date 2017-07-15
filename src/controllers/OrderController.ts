a43110cb7851f3d8934a17abeb03e6edb080fbf0
Mdrbhatti <Mdrbhatti@gmail.com>
2017-07-15 22:41:45 +0200

    refactor: remove comments

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -4,27 +4,3 @@ import { IPublishedItem } from "../interfaces/IPublishedItem";
 import { PublishedItem } from "../models/PublishedItemModel";
 import { Request, Response, NextFunction } from "express";
 import * as mongoose from "mongoose";
-
-// export const getOrder = (req: Request, res: Response) => {
-//     // Find all
-//     Order.find({}, function (err, orders: IOrder[]) {
-//         console.log(orders);
-//     });
-// };
-
-// export const postOrder = (req: Request, res: Response, next: NextFunction) => {
-//     // Create order
-//     // Get published item
-//     // Get user identity
-
-//     // const user: IUser = new User(_.extend(req.body, { lastLogin: new Date(), createdOn: new Date() }));
-// PublishedItem.findOne({ username: req.body.username }, function (err: mongoose.Error, item: IPublishedItem) {
-//         if (err || !item) {
-//             return res.status(400).send({ message: "Item doesn't exist" });
-//         }
-//         else {
-
-//         }
-//     });
-
-// };
