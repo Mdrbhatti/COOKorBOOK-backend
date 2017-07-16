539008737d8cc9df65dc4767c8ab3a2a4568160d
H804281 <oystein.hauan@eika.no>
2017-07-16 21:47:26 +0200

    wip

--- src/controllers/ItemController.ts
+++ src/controllers/ItemController.ts
@@ -161,7 +161,7 @@ export const getPublishedItemsForSeller = (req: Request, res: Response) => {
 
 
 
-  PublishedItem.find({"seller": getUserIdFromJwt(req)}, {"servings": 1, "price": 1, "item.title": 1, "seller.username": 1}, function (err: mongoose.Error, items: IPublishedItem[]) {
+  PublishedItem.find({"seller": getUserIdFromJwt(req)}, 'price servings item.title', function (err: mongoose.Error, items: IPublishedItem[]) {
     if (err || items.length == 0) {
       res.status(400).send({ message: "Can't find any published items" });
     } else {
